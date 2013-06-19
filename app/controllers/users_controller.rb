ebbbe6617d48da8e93bd45e5994291b7ce44fdda
Jenny Goff <jennyg@effectivephilanthropy.org>
2013-06-19 11:28:33 -0400

    chapter 7 cleaning up, did a db:test:prepare

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-	def new
-	end
-
   def new
-    @user = User.new
+   @user = User.new
   end
 
 def create
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,7 +13,7 @@ describe "User pages" do
   end
 
   describe "signup page" do
-    before { visit signup_path }
+   before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
